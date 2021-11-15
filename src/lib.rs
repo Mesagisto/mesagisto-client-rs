1eb82b620623cfa8130de1414896dfedb2739377
itsusinn <itsusinn@foxmail.com>
2021-11-15 16:00:02 +0800

    feat: ConfigBuilder

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
 #![feature(fn_traits)]
+use arcstr::ArcStr;
+use educe::Educe;
+use futures::future::BoxFuture;
 use once_cell::sync::OnceCell;
+use sled::IVec;
+use db::DB;
+use cache::CACHE;
+use cipher::CIPHER;
+use res::RES;
+use server::SERVER;
+use net::NET;
 
 pub mod cache;
 pub mod cipher;
@@ -12,6 +22,87 @@ pub mod server;
 
 #[macro_use]
 extern crate singleton;
+
+type Handler =
+  dyn Fn(&(Vec<u8>, IVec)) -> BoxFuture<anyhow::Result<ArcStr>> + Send + Sync + 'static;
+
+#[derive(Educe)]
+#[educe(Default)]
+pub struct MesagistoConfig {
+  #[educe(Default = "default")]
+  pub name: ArcStr,
+  pub proxy: Option<ArcStr>,
+  #[educe(Default = true)]
+  pub cipher_enable: bool,
+  pub cipher_key: ArcStr,
+  pub cipher_refuse_plain: bool,
+  #[educe(Default = "nats://itsusinn.site:4222")]
+  pub nats_address: ArcStr,
+  pub photo_url_resolver: Option<Box<Handler>>,
+}
+impl MesagistoConfig {
+  pub fn builder() -> MesagistoConfigBuilder {
+    MesagistoConfigBuilder::new()
+  }
+  pub async fn apply(self) {
+    DB.init(self.name.some());
+    CACHE.init();
+    if self.cipher_enable {
+      CIPHER.init(&self.cipher_key, &self.cipher_refuse_plain);
+    } else {
+      CIPHER.deinit();
+    }
+
+    RES.init().await;
+    RES.photo_url_resolver.init(self.photo_url_resolver.unwrap());
+    SERVER.init(&self.nats_address).await;
+    NET.init(self.proxy);
+  }
+}
+#[derive(Default)]
+pub struct MesagistoConfigBuilder {
+  config: MesagistoConfig,
+}
+impl MesagistoConfigBuilder {
+  pub fn new() -> Self {
+    Self::default()
+  }
+  pub fn name(mut self, name: impl Into<ArcStr>) -> Self {
+    self.config.name = name.into();
+    self
+  }
+  pub fn proxy(mut self, proxy: Option<ArcStr>) -> Self {
+    self.config.proxy = proxy;
+    self
+  }
+  pub fn cipher_enable(mut self, enable: bool) -> Self {
+    self.config.cipher_enable = enable;
+    self
+  }
+  pub fn cipher_key(mut self, key: impl Into<ArcStr>) -> Self {
+    self.config.cipher_key = key.into();
+    self
+  }
+  pub fn cipher_refuse_plain(mut self, refuse: bool) -> Self {
+    self.config.cipher_refuse_plain = refuse;
+    self
+  }
+  pub fn nats_address(mut self, address: impl Into<ArcStr>) -> Self {
+    self.config.nats_address = address.into();
+    self
+  }
+  pub fn photo_url_resolver<F>(
+    mut self,
+    resolver: F,
+  ) -> Self  where F: Fn(&(Vec<u8>, IVec)) -> BoxFuture<anyhow::Result<ArcStr>> + Send + Sync + 'static {
+    let h = Box::new(resolver);
+    self.config.photo_url_resolver = Some(h);
+    self
+  }
+  pub fn build(self) -> MesagistoConfig {
+    self.config
+  }
+}
 
 #[derive(Debug)]
 pub struct LateInit<T> {
