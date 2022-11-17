63c7618ee995f95ebcb97fbce81618d5254b78ae
Itsusinn <itsusinn@meowcat.org>
2022-11-17 19:16:03 +0800

    refactor: code clean

--- src/res.rs
+++ src/res.rs
@@ -1,13 +1,21 @@
-use std::{path::PathBuf, time::Duration};
+use std::{panic, path::PathBuf, sync::Arc, time::Duration};
 
 use arcstr::ArcStr;
 use color_eyre::eyre::Result;
 use dashmap::DashMap;
 use lateinit::LateInit;
 use sled::IVec;
-use tokio::{sync::oneshot, task::JoinHandle};
+use tokio::{sync::oneshot, task::JoinHandle, time::timeout};
+use tracing::trace;
+use uuid::Uuid;
 
-use crate::{db::DB, ResultExt};
+use crate::{
+  data::{events::Event, Packet},
+  db::DB,
+  net::NET,
+  server::SERVER,
+  EitherExt, ResultExt,
+};
 
 #[derive(Singleton, Default)]
 pub struct Res {
@@ -15,6 +23,17 @@ pub struct Res {
   pub handlers: DashMap<ArcStr, Vec<oneshot::Sender<PathBuf>>>,
 }
 impl Res {
+  pub async fn init(&self) {
+    let path = {
+      let mut dir = std::env::temp_dir();
+      dir.push("mesagisto");
+      dir
+    };
+    tokio::fs::create_dir_all(path.as_path()).await.unwrap();
+    self.directory.init(path);
+    RES.poll().await;
+  }
+
   async fn poll(&self) {
     let _: JoinHandle<_> = tokio::spawn(async {
       let mut interval = tokio::time::interval(Duration::from_millis(200));
@@ -57,22 +76,83 @@ impl Res {
     Ok(path)
   }
 
-  pub async fn init(&self) {
-    let path = {
-      let mut dir = std::env::temp_dir();
-      dir.push("mesagisto");
-      dir
-    };
-    tokio::fs::create_dir_all(path.as_path()).await.unwrap();
-    self.directory.init(path);
-    RES.poll().await;
-  }
-
   pub fn put_image_id<U, F>(&self, uid: U, file_id: F)
   where
     U: AsRef<[u8]>,
     F: Into<IVec>,
   {
     DB.put_image_id(uid, file_id);
+  }
+
+  pub async fn file(
+    &self,
+    id: &Vec<u8>,
+    url: &Option<ArcStr>,
+    room: Arc<Uuid>,
+    server: &ArcStr,
+  ) -> Result<PathBuf> {
+    match url {
+      Some(url) => self.file_by_url(id, url).await,
+      None => self.file_by_uid(id, room, server).await,
+    }
+  }
+
+  pub async fn file_by_uid(
+    &self,
+    uid: &Vec<u8>,
+    room: Arc<Uuid>,
+    server: &ArcStr,
+  ) -> Result<PathBuf> {
+    let uid_str: ArcStr = base64_url::encode(uid).into();
+    trace!("Caching file by uid {}", uid_str);
+    let path = RES.path(&uid_str);
+    if path.exists() {
+      trace!("File exists,return the path");
+      return Ok(path);
+    }
+    let tmp_path = RES.tmp_path(&uid_str);
+    if tmp_path.exists() {
+      trace!("TmpFile exists,waiting for the file downloading");
+      return RES.wait_for(&uid_str).await;
+    }
+    trace!("TmpFile dont exist,requesting image url");
+    let event: Event = Event::RequestImage { id: uid.clone() };
+    // fixme error handling
+    let packet = Packet::new(room, event.to_right())?;
+    // fixme timeout check
+    let packet = timeout(Duration::from_secs(7), SERVER.request(packet, server)).await??;
+
+    match packet.decrypt()? {
+      either::Either::Right(event) => match event {
+        Event::RespondImage { id, url } => self.file_by_url(&id, &url).await,
+        _ => panic!("Not correct response"),
+      },
+      either::Either::Left(_) => panic!("Not correct response"),
+    }
+  }
+
+  pub async fn file_by_url(&self, id: &Vec<u8>, url: &ArcStr) -> Result<PathBuf> {
+    let id_str: ArcStr = base64_url::encode(id).into();
+    let path = RES.path(&id_str);
+    if path.exists() {
+      return Ok(path);
+    }
+
+    let tmp_path = RES.tmp_path(&id_str);
+    if tmp_path.exists() {
+      Ok(RES.wait_for(&id_str).await?)
+    } else {
+      // fixme error handling
+      NET.download(url, &tmp_path).await?;
+      tokio::fs::rename(&tmp_path, &path).await?;
+      Ok(path)
+    }
+  }
+
+  pub async fn put_file(&self, id: &Vec<u8>, file: &PathBuf) -> Result<PathBuf> {
+    let id_str: ArcStr = base64_url::encode(id).into();
+    let path = RES.path(&id_str);
+    tokio::fs::rename(&file, &path).await?;
+    Ok(path)
   }
 }
