c8b74014a274f3c8610ac4d1e133985c83637e89
Itsusinn逸新 <itsusinn@meowcat.org>
2024-01-29 03:49:37 +0800

    refactor: change ase-gcm into aes-gcm-siv which have nonce-resist
    aes gcm siv能抵御nonce重放攻击

--- src/cipher.rs
+++ src/cipher.rs
@@ -1,24 +1,21 @@
 use std::ops::Deref;
 
-use aes_gcm::{
-  aead::generic_array::GenericArray, aes::Aes256, Aes256Gcm, AesGcm, KeyInit, KeySizeUser,
-};
+use aes::Aes256;
+use aes_gcm_siv::{Aes256GcmSiv, AesGcmSiv, KeyInit};
 use arcstr::ArcStr;
 use color_eyre::eyre::Result;
 use lateinit::LateInit;
-use typenum::U12;
-
-pub type Key<B> = GenericArray<u8, <B as KeySizeUser>::KeySize>;
 
 #[derive(Singleton, Default)]
 pub struct Cipher {
-  inner: LateInit<AesGcm<Aes256, U12>>,
-  pub key: LateInit<Key<Aes256Gcm>>,
+  inner: LateInit<AesGcmSiv<Aes256>>,
+  pub key: LateInit<[u8; 32]>,
+  pub nonce: LateInit<aes_gcm_siv::Nonce>,
   pub origin_key: LateInit<ArcStr>,
 }
 
 impl Deref for Cipher {
-  type Target = AesGcm<Aes256, U12>;
+  type Target = AesGcmSiv<Aes256>;
 
   fn deref(&self) -> &Self::Target {
     &self.inner
@@ -27,24 +24,14 @@ impl Deref for Cipher {
 
 impl Cipher {
   pub fn init(&self, key: &ArcStr) -> Result<()> {
-    let hash_key = {
-      use sha2::{Digest, Sha256};
-      self.origin_key.init(key.to_owned());
-      let mut hasher = Sha256::new();
-      hasher.update(key.as_bytes());
-      hasher.finalize()
-    };
+    use sha2::{Digest, Sha256};
+    self.origin_key.init(key.to_owned());
+    let hash_key = Sha256::digest(key).into();
     self.key.init(hash_key);
-    let cipher = Aes256Gcm::new_from_slice(self.key.as_slice())?;
+    let nonce = aes_gcm_siv::Nonce::from_slice(hash_key[0..12].try_into().unwrap()).to_owned();
+    self.nonce.init(nonce);
+    let cipher = Aes256GcmSiv::new_from_slice(self.key.as_slice())?;
     self.inner.init(cipher);
     Ok(())
-  }
-
-  pub fn new_nonce(&self) -> [u8; 12] {
-    use rand::RngCore;
-    let mut rng = rand::thread_rng();
-    let mut nonce = [0u8; 12];
-    rng.fill_bytes(&mut nonce);
-    nonce
   }
 }
